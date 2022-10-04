8686444857bb836c775d7538d130b8f1d90eaaf8
Brenno Lemos <brenno@syndel.is>
2022-10-04 20:29:21 -0300

    mouse and sound examples

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include <jogo.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdbool.h>
 
 int main() {
 
     abre_janela(640, 480);
+    toca_som_em_loop("assets/awesomeness.wav");
 
     while (janela_esta_aberta()) {
         cor(AMARELO);
@@ -17,9 +18,12 @@ int main() {
 
         desenha_imagem(320, 240, "assets/guy.gif");
 
-        if (tecla_apertada(ESC))
+        if (tecla_acabou_de_ser_apertada(ESC))
             janela_deve_fechar();
 
+        if (botao_mouse_acabou_de_ser_apertado(BOTAO_ESQUERDO))
+            recomeca_som("assets/jump.wav");
+        
     }
 
     fecha_janela();
