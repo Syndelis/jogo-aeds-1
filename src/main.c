810a42e0aebed4e294b96d3959486208e13d618c
Brenno Lemos <brenno@syndel.is>
2022-10-07 19:18:04 -0300

    added `preenchimento`, `rotacao` and `zoom` examples

--- src/main.c
+++ src/main.c
@@ -8,10 +8,22 @@ int main() {
     abre_janela(640, 480);
     toca_som_em_loop("assets/awesomeness.wav");
 
+    double fator_zoom = 1;
+
     while (janela_esta_aberta()) {
+
         cor(AMARELO);
+        preenchimento(false);
+
         desenha_retangulo(0, 0, 320, 240);
+
+        zoom(mouse_x, mouse_y, fator_zoom);
         desenha_imagem(mouse_x, mouse_y, "assets/tux.png");
+        reseta_zoom();
+
+        cor(VERMELHO);
+        preenchimento(true);
+        desenha_circulo(200, 200, 10);
 
         fonte("assets/ubuntu.ttf", 32);
         desenha_texto(20, 20, "Esse é um teste!");
@@ -22,8 +34,10 @@ int main() {
             janela_deve_fechar();
 
         if (botao_mouse_acabou_de_ser_apertado(BOTAO_ESQUERDO))
-            recomeca_som("assets/jump.wav");
+            toca_som("assets/jump.wav");
         
+        fator_zoom += roda_do_mouse * 0.1;
+
     }
 
     fecha_janela();
